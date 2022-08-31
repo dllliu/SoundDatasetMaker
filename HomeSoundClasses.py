32e734d0a789b9fa6d3f972855ddb53c4fc4b8ce
dliu <dliu30@stuy.edu>
2022-08-30 22:38:16 -0400

    untested with function

--- HomeSoundClasses.py
+++ HomeSoundClasses.py
@@ -19,11 +19,19 @@ client.set_token(token,"token")
 
 DataOuptut=[]
 
-TARGETITEMCOUNT=50
+TARGETITEMCOUNT=0
 
 TotalItemCount=0
 
-finished=False
+finished = False
+
+def reset():
+    TotalItemCount=0
+    finished=False
+return None
+
+reset()
+TARGETITEMCOUNT = 50
 page = client.text_search(query="talking",fields="id,name,previews,duration,username,tags,description,geotag,license,url")
 nextPage = page
 while nextPage!=None:
@@ -53,376 +61,113 @@ while nextPage!=None:
 
 print("Download of Human Voice Class Completed")
 
-def reset():
-    TotalItemCount=0
-    finished=False
+
+def DownloadDictOfSoundResults(arr):
+    for x in items_arr:
+        page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
+        nextPage = page
+        while nextPage!=None:
+            for sound in page:
+                if "\\" in sound.name:
+                    continue
+                tags = str(sound.tags)
+                New_Tags = tags.replace(",","+")
+                rowDictionary={
+                "name":sound.name,
+                "url":sound.url,
+                "tags":New_Tags,
+                "username":sound.username,
+                "license":sound.license,
+                "description":sound.description,
+                "duration":str(sound.duration),
+                "geotags":sound.geotag
+                }
+                sound.retrieve_preview(".",sound.name)
+                DataOuptut.append(rowDictionary)
+                TotalItemCount+=1
+                if(TotalItemCount>=TARGETITEMCOUNT):
+                    finished=True
+            if(finished):
+                break
+            nextPage = page.next_page()
 return None
 
 reset()
 TARGETITEMCOUNT=20
 
 mvmnt_arr = ["home walking","house eating","setting table","washing dishes","cutting food"]
-for x in mvmnt_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+DownloadDictOfSoundResults(mvmnt_arr)
 
+print("Phase 1 of Movement Done")
 
 reset()
 items_arr = ["box moving","coins","match","keys","velcro","zippers,","box moving"]
 TARGETITEMCOUNT=int(100/items_arr.length)
+DownloadDictOfSoundResults(items_arr)
 
-for x in items_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+print("Phase 2 of Movement Done")
 
 reset()
 bev_arr = ["ice cubes","beverage"]
 TARGETITEMCOUNT=int(100/bev_arr.length)
+DownloadDictOfSoundResults(bev_arr)
 
-for x in bev_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+print("Final Phase 3 of Movement Done")
 
-print("Movement Class Downloaded")
 
 reset()
 Hyg_arr = ["hygiene","vaccum","shaving","cleaning home","brushing teeth","washing clothes"]
 TARGETITEMCOUNT=int(100/Hyg_arr.length)
+DownloadDictOfSoundResults(Hyg_arr)
 
-for x in Hyg_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
 
 reset()
-ntitems_arr = ["weather","hail","thunder"]
-TARGETITEMCOUNT=int(100/ntitems_arr.length)
-for x in Hyg_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+nature_arr = ["weather","hail","thunder"]
+TARGETITEMCOUNT=int(100/nature_arr.length)
+DownloadDictOfSoundResults(nature_arr)
 
 print("Nature Class Downloaded")
 
 reset()
 anm_arr = ["bark","mews","tweet"]
 TARGETITEMCOUNT=int(100/anm_arr.length)
-for x in anm_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
-#-----------------------------------
+DownloadDictOfSoundResults(nature_arr)
 
 reset()
-PAplc_arr = ["cell phone","camera","computer"]
-TARGETITEMCOUNT=int(100/PAplc.length)
-for x in anm_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+PersonalAppliances = ["cell phone","camera","computer"]
+TARGETITEMCOUNT=int(100/PersonalAppliances.length)
+DownloadDictOfSoundResults(PersonalAppliances)
 
 print("Personal Appliance Class Finished Downloading")
 
 reset()
-KitAppArr = ["kitchen appliances","oven","blender","garbage disposal","stove","toaster","furnace"]
-TARGETITEMCOUNT=int(100/KitAppArr.length)
-for x in KitAppArr_arr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+KitchenApp = ["kitchen appliances","oven","blender","garbage disposal","stove","toaster","furnace"]
+TARGETITEMCOUNT=int(100/KitchenApp.length)
+DownloadDictOfSoundResults(KitchenApp)
 
 print("Kitchen Appliance Class Finished Downloading")
 
 reset()
-LivApp = ["Sofa","TV","Fan","Light Switch","Blinds"]
-TARGETITEMCOUNT=int(100/LivApp.length)
-
-for x in LivApp:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+LivingApp = ["Sofa","TV","Fan","Light Switch","Blinds"]
+TARGETITEMCOUNT=int(100/LivingApp.length)
+DownloadDictOfSoundResults(LivingApp)
 
 print("Living Room Appliance Class Finished Downloading")
 
 reset()
-BanoApp = ["bathroom","ventilator","hair dryer","soap dispenser"]
-TARGETITEMCOUNT=int(100/BanoApp.length)
-
-for x in LivApp:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+Bathroom = ["bathroom","ventilator","hair dryer","soap dispenser"]
+TARGETITEMCOUNT=int(100/Bathroom.length)
+DownloadDictOfSoundResults(Bathroom)
 
 print("Bathroom Appliance Class Finished Downloading")
-
 
 reset()
 OfficeApp = ["office","keyboard","printer","Light Switch Office"]
 TARGETITEMCOUNT=int(100/OfficeApp.length)
-
-for x in OfficeApp:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
-
+DownloadDictOfSoundResults(OfficeApp)
 
 reset()
+
 TARGETITEMCOUNT=50
 page = client.text_search(query="bed",fields="id,name,previews,duration,username,tags,description,geotag,license,url")
 nextPage = page
@@ -458,68 +203,15 @@ print("Bedroom Appliance Class Finished Downloading")
 reset()
 FurnArr = ["furniture","carpet","closet","cabinet","chair"]
 TARGETITEMCOUNT=int(100/FurnArr.length)
+DownloadDictOfSoundResults(FurnArr)
 
-for x in FurnArr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
 
 print("Furniture Class Finished Downloading")
 
 reset()
-InsArr = ["guitar","piano","violin","drums","saxophone","flute"]
+InstrumentsArr = ["guitar","piano","violin","drums","saxophone","flute"]
 TARGETITEMCOUNT=int(100/InsArr.length)
-
-for x in InsArr:
-    page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
-    nextPage = page
-    while nextPage!=None:
-        for sound in page:
-            if "\\" in sound.name:
-                continue
-            tags = str(sound.tags)
-            New_Tags = tags.replace(",","+")
-            rowDictionary={
-            "name":sound.name,
-            "url":sound.url,
-            "tags":New_Tags,
-            "username":sound.username,
-            "license":sound.license,
-            "description":sound.description,
-            "duration":str(sound.duration),
-            "geotags":sound.geotag
-            }
-            sound.retrieve_preview(".",sound.name)
-            DataOuptut.append(rowDictionary)
-            TotalItemCount+=1
-            if(TotalItemCount>=TARGETITEMCOUNT):
-                finished=True
-        if(finished):
-            break
-        nextPage = page.next_page()
+DownloadDictOfSoundResults(InstrumentsArr)
 
 print("Instrument Class Finished Downloading")
 print("Finished Downloads!!!!!!!!")
