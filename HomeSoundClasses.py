958ed756d02f839dcb535233fddd1f4d17ed365b
dliu <dliu30@stuy.edu>
2022-09-24 00:12:49 -0400

    changed code for updated dataset

--- HomeSoundClasses.py
+++ HomeSoundClasses.py
@@ -18,50 +18,33 @@ client = freesound.FreesoundClient()
 client.set_token(token,"token")
 #Voice Class
 
-DataOuptut=[]
+Dataoutput=[]
 
-TARGETITEMCOUNT=60
+TARGETITEMCOUNT= 0
 
 TotalItemCount=0
 
 finished = False
 
-def setTarget(target):
-    global TARGETITEMCOUNT
-    TARGETITEMCOUNT = target
-
-def setTotal(total):
-    global TotalItemCount
-    TotalItemCount = total
-
-def setFinished(completed):
-    global finished
-    finished = completed
-
-def getFinished():
-    global finished
-    return finished
-
-def IncrementTarget():
-    global TARGETITEMCOUNT
-    TARGETITEMCOUNT += 1
-
-def IncrementTotal():
-    global TotalItemCount
-    TotalItemCount += 1
-
-
-def DownloadDictOfSoundResults(arr,target):
+SOURCE_DATA = 'AudioData'
+def DownloadDictOfSoundResults(arr,dir):
+    count = 0
+    finished = False
+    temp = []
     for x in arr:
         page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
         for sound in page:
-            if "\\" in sound.name or sound.duration <= 5:
+            #blacklisted user for uploading incorrectly classified sounds
+            if sound.duration <= 5 or str(sound.username) == "Duisterwho" or "\\" in str(sound.name) or "/" in str(sound.name):
+                #print("Skipped" + str(sound))
                 continue
             tags = str(sound.tags)
             New_Tags = tags.replace(",","+")
+            Name = str(sound.name)
+            new_Name = Name.replace("\\","-")
             uuid = uuid4()
             rowDictionary={
-            "name":sound.name,
+            "name":new_Name,
             "url":sound.url,
             "uuid":uuid,
             "tags":New_Tags,
@@ -71,257 +54,213 @@ def DownloadDictOfSoundResults(arr,target):
             "duration":str(sound.duration),
             "geotags":sound.geotag
             }
-            sound.retrieve_preview(".",str(uuid)+".wav")
-            DataOuptut.append(rowDictionary)
-            IncrementTotal()
-            print(TotalItemCount,sound.name)
+            sound.retrieve_preview(".",os.path.join(SOURCE_DATA,dir,new_Name+".wav"))
+            count += 1
+            Dataoutput.append(rowDictionary)
+            print(str(count) + ": " + new_Name,sound.username)
 
-        if page.next_page() != None:
-            nextPage = page.next_page()
-        while nextPage!=None:
-            for sound in nextPage:
-                if "\\" in sound.name or sound.duration <= 5:
-                    continue
-                tags = str(sound.tags)
-                New_Tags = tags.replace(",","+")
-                uuid = uuid4()
-                sound.retrieve_preview(".",str(uuid)+".wav")
-                DataOuptut.append(rowDictionary)
-                IncrementTotal()
-                print(TotalItemCount,sound.name)
-                if(TotalItemCount>=target):
-                    setFinished(True)
-            if(getFinished()):
-                break
-            nextPage = nextPage.next_page()
-
-#Code to Download Voice Class
-
-
-voice_arr = ["talking"]
-DownloadDictOfSoundResults(voice_arr,60)
-print("Download of Human Voice Class Completed")
-with open('Voices.csv', 'w') as csvfile:
-    fieldnames = DataOuptut[0].keys()
+def DownloadNextPage(arr,dir):
+    looped_each_count = 0
+    count = 0
+    for x in arr:
+        page = client.text_search(query=x,fields="id,name,previews,duration,username,tags,description,geotag,license,url")
+        nextPage = page.next_page()
+        for sound in nextPage:
+            if sound.duration <= 5 or str(sound.username) == "Duisterwho" or "\\" in str(sound.name) or "/" in str(sound.name):
+            #print("Skipped" + str(sound))
+                continue
+            tags = str(sound.tags)
+            New_Tags = tags.replace(",","+")
+            Name = str(sound.name)
+            new_Name = Name.replace("\\","-")
+            uuid = uuid4()
+            rowDictionaryPaged={
+            "name":new_Name,
+            "url":sound.url,
+            "uuid":uuid,
+            "tags":New_Tags,
+            "username":sound.username,
+            "license":sound.license,
+            "description":sound.description,
+            "duration":str(sound.duration),
+            "geotags":sound.geotag
+            }
+            sound.retrieve_preview(".",os.path.join(SOURCE_DATA,dir,new_Name+".wav"))
+            Dataoutput.append(rowDictionaryPaged)
+            count += 1
+            print(str(count) + ": " + new_Name,sound.url)
+"""
+voice_arr = ["speaking","laughing","shouting","crying","coughing"]
+DownloadDictOfSoundResults(voice_arr,'001 - Voices')
+DownloadNextPage(voice_arr,'001 - Voices')
+with open('Voices.csv', 'w',encoding='utf-8') as csvfile:
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
 
-print(DataOuptut)
-DataOuptut.clear()
-print(DataOuptut)
-
-setFinished(False)
-setTotal(0)
-mvmnt_arr = ["home walking","house eating","clapping","kids fighting home","home movement"]
-try:
-    DownloadDictOfSoundResults(mvmnt_arr,int(100/len(mvmnt_arr)))
-except ValueError:
-    print("next page is not accessible")
-finally:
-    with open('Actions.csv', 'w') as csvfile:
-        fieldnames = DataOuptut[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
-        writer.writeheader()
-        for row in DataOuptut:
-            writer.writerow(row);
-
-print("Phase 1 of Movement Done")
-
-
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-items_arr = ["box moving","coins","plastic bags","keys","velcro","zippers,"]
-targetEachEntry=int(100/len(items_arr))
-try:
-    DownloadDictOfSoundResults(items_arr,targetEachEntry)
-except ValueError:
-    print("next page is not accessible")
-finally:
-    with open('EverydayItems.csv', 'w') as csvfile:
-        fieldnames = DataOuptut[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
-        writer.writeheader()
-        for row in DataOuptut:
-            writer.writerow(row);
-
-print("Phase 2 of Movement Done")
-
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-bev_arr = ["ice cubes","beverage"]
-targetEachEntry=int(100/len(bev_arr))
-DownloadDictOfSoundResults(bev_arr,targetEachEntry)
-with open('Beverages.csv', 'w') as csvfile:
-    fieldnames = DataOuptut[0].keys()
+Dataoutput.clear()
+bev_arr = ["splash","pour","drip","fill","boil"]
+DownloadDictOfSoundResults(bev_arr,'002 - Liquids')
+DownloadNextPage(bev_arr,'002 - Liquids')
+with open('Liquids.csv', 'w',encoding='utf-8') as csvfile:
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
-print("Final Phase 3 of Movement Done")
 
 
+Dataoutput.clear()
+locomotion_arr = ["walking","clapping","snapping","running"]
+DownloadDictOfSoundResults(locomotion_arr,'003 - Locomotion')
+DownloadNextPage(locomotion_arr,'003 - Locomotion')
+with open('Motion.csv', 'w',encoding='utf-8') as csvfile:
+    fieldnames = Dataoutput[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
+    writer.writeheader()
+    for row in Dataoutput:
+        writer.writerow(row);
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-Hyg_arr = ["hygiene","vaccum","shaving","cleaning home","washing clothes"]
-DownloadDictOfSoundResults(Hyg_arr,15)
+Dataoutput.clear()
+items_arr = ["box moving","coins","plastic bags","keys","velcro","silverware,","cans"]
+DownloadDictOfSoundResults(items_arr,'004 - HouseItems')
+DownloadNextPage(items_arr,'004 - HouseItems')
+with open('HouseholdItems.csv', 'w',encoding='utf-8') as csvfile:
+    fieldnames = Dataoutput[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
+    writer.writeheader()
+    for row in Dataoutput:
+        writer.writerow(row);
+
+Dataoutput.clear()
+digestive = ["chewing","biting","gargling","hiccuping"]
+DownloadDictOfSoundResults(digestive,'005 - Digestive')
+DownloadNextPage(digestive,'005 - Digestive')
+with open('Locomotion.csv', 'w',encoding='utf-8') as csvfile:
+    fieldnames = Dataoutput[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
+    writer.writeheader()
+    for row in Dataoutput:
+        writer.writerow(row);
+
+Dataoutput.clear()
+Hyg_arr = ["hygiene","vaccum","shaving","cleaning home","aerosol","hair spray"]
+DownloadDictOfSoundResults(Hyg_arr,'006 - Hygiene')
+DownloadNextPage(Hyg_arr,'006 - Hygiene')
 with open('Hygiene.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
+"""
 
-
-
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-nature_arr = ["weather","home rain","thunder"]
-targetEachEntry=int(100/len(nature_arr))
-DownloadDictOfSoundResults(nature_arr,targetEachEntry)
+Dataoutput.clear()
+nature_arr = ["gusts","rain","thunder","hail"]
+DownloadDictOfSoundResults(nature_arr,'007 - Elements')
+DownloadNextPage(nature_arr,'007 - Elements')
 with open('Nature.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Nature Class Downloaded")
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-anm_arr = ["bark","mews","tweet"]
-targetEachEntry=int(100/len(anm_arr))
-DownloadDictOfSoundResults(anm_arr,targetEachEntry)
+Dataoutput.clear()
+anm_arr = ["bark","mews","tweet","howl"]
+DownloadDictOfSoundResults(anm_arr,'008 - Animals')
+DownloadNextPage(anm_arr,'008 - Animals')
 with open('Animals.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-PersonalAppliances = ["cell phone","camera","computer"]
-targetEachEntry=int(100/len(PersonalAppliances))
-DownloadDictOfSoundResults(PersonalAppliances,targetEachEntry)
-with open('PersonalAppliances.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+Dataoutput.clear()
+Cooking = ["microwave","oven","refrigerator","stove","toaster","kettle"]
+DownloadDictOfSoundResults(Cooking,'009 - Cooking_Appliances')
+DownloadNextPage(Cooking,'009 - Cooking_Appliances')
+with open('Cooking_Appliances.csv', 'w', encoding="utf-8") as csvfile:
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Personal Appliance Class Finished Downloading")
+Dataoutput.clear()
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-KitchenApp = ["kitchen appliances","oven","blender","garbage disposal","stove","toaster","furnace"]
-targetEachEntry=int(100/len(KitchenApp))
-DownloadDictOfSoundResults(KitchenApp,targetEachEntry)
-with open('Kitchen.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+Cleaning = ["dishwasher","washer","dryer","vaccum"]
+DownloadDictOfSoundResults(Cleaning,'010 - Cleaning_Appliances')
+DownloadNextPage(Cleaning,'010 - Cleaning_Appliances')
+with open('Cleaning_Appliances.csv', 'w', encoding="utf-8") as csvfile:
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Kitchen Appliance Class Finished Downloading")
+Dataoutput.clear()
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-LivingApp = ["Sofa","TV","Fan","Light Switch","Blinds"]
-targetEachEntry=int(100/len(LivingApp))
-DownloadDictOfSoundResults(LivingApp,targetEachEntry)
-with open('LivingRoom.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+lighting = ["light switch","flashlight","lamp"]
+DownloadDictOfSoundResults(lighting, '011 - LightingAppliances')
+DownloadNextPage(lighting, '011 - LightingAppliances')
+with open('Lighting.csv', 'w', encoding="utf-8") as csvfile:
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Living Room Appliance Class Finished Downloading")
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-Bathroom = ["bathroom","ventilator","hair dryer","soap dispenser"]
-targetEachEntry=int(100/len(Bathroom))
-DownloadDictOfSoundResults(Bathroom,targetEachEntry)
-with open('Bathroom.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+Dataoutput.clear()
+
+ventilation = ["fans","heater","air conditioner","home ventilation"]
+DownloadDictOfSoundResults(ventilation,'012 - Ventilation_Appliances')
+DownloadNextPage(ventilation,'012 - Ventilation_Appliances')
+with open('Ventilation.csv', 'w', encoding="utf-8") as csvfile:
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Bathroom Appliance Class Finished Downloading")
-
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-OfficeApp = ["office","keyboard","printer","Light Switch Office"]
-targetEachEntry=int(100/len(OfficeApp))
-DownloadDictOfSoundResults(OfficeApp,targetEachEntry)
-with open('Office.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
-    writer.writeheader()
-    for row in DataOuptut:
-        writer.writerow(row);
-
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-bed_arr = ["bed","bedroom"]
-DownloadDictOfSoundResults(bed_arr,60)
-with open('Bedroom.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
-    writer.writeheader()
-    for row in DataOuptut:
-        writer.writerow(row);
-
-print("Bedroom Appliance Class Finished Downloading")
-
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-FurnArr = ["furniture","carpet","closet","cabinet","chair"]
-targetEachEntry=int(100/len(FurnArr))
-DownloadDictOfSoundResults(FurnArr,targetEachEntry)
+Dataoutput.clear()
+furniture = ["sofa","door","cabinet","chair","bed","drawers","closet"]
+DownloadDictOfSoundResults(furniture,'013 - Furniture')
+DownloadNextPage(furniture,'013 - Furniture')
 with open('Furniture.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Furniture Class Finished Downloading")
+Dataoutput.clear()
+tools = ["hammer","saw","screwdriver","power dril"]
+DownloadDictOfSoundResults(tools, '014 - Tools')
+DownloadNextPage(tools,'014 - Tools')
+with open('Tools.csv', 'w', encoding="utf-8") as csvfile:
+    fieldnames = Dataoutput[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
+    writer.writeheader()
+    for row in Dataoutput:
+        writer.writerow(row);
 
-DataOuptut.clear()
-setFinished(False)
-setTotal(0)
-InstrumentsArr = ["guitar","piano","violin","drums","saxophone","flute"]
-targetEachEntry=int(100/len(InstrumentsArr))
-DownloadDictOfSoundResults(InstrumentsArr,targetEachEntry)
+
+Dataoutput.clear()
+InstrumentsArr = ["guitar","piano","flute","trumpet","saxophone"]
+DownloadDictOfSoundResults(InstrumentsArr,'015 - Instruments')
+DownloadNextPage(InstrumentsArr,'015 - Instruments')
 with open('Instruments.csv', 'w', encoding="utf-8") as csvfile:
-    fieldnames = DataOuptut[0].keys()
+    fieldnames = Dataoutput[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
     writer.writeheader()
-    for row in DataOuptut:
+    for row in Dataoutput:
         writer.writerow(row);
 
-print("Instrument Class Finished Downloading")
 print("Finished Downloads!!!!!!!!")
