8beaedeff864bfa797cb840778f09fdfa4d09529
dliu <dliu30@stuy.edu>
2022-10-22 23:35:36 -0400

    class-fold distribution

--- ProcessDataset.py
+++ ProcessDataset.py
@@ -10,9 +10,9 @@ import ffmpeg
 import matplotlib.pyplot as plt
 #Alrady Ran to Convert Files
 
-#parent_folder = #OG SOURCE DIR WHERE ALL SUBFOLDERS OF FILE CLASSES WERE SAVED
+parent_folder = 'AudioData'#OG SOURCE DIR WHERE ALL SUBFOLDERS OF FILE CLASSES WERE SAVED
 
-#copy_parent_folder = #COPY DIRS FOR EACH STEP
+copy_parent_folder = 'Copy-AudioData'#COPY DIRS FOR EACH STEP
 
 def ConvertFilesWithFFmpeg(input_dir,output_dir):
     files = os.listdir(input_dir)
@@ -47,6 +47,7 @@ def findSum(input,output):
         for files in os.listdir(os.path.join(output,copy_parent_dir)):
             count_files_copy += 1
     return count_files_og, count_files_copy
+
 
 def GenerateWAVE(input_dir,output_dir):
     files = os.listdir(input_dir)
@@ -92,10 +93,12 @@ def categorizeFiles(input_dir):
 #make file structure for folded data(eg: fold1, fold2...)
 
 ###   
+'''
 temp_arr = []
 make_file_structure()
 for subdir in os.listdir(parent_folder):
-    categorizeFiles(subdir) 
+    categorizeFiles(subdir)
+''' 
 ####
 
 
@@ -163,3 +166,61 @@ plt.xlabel("Class Number")
 plt.ylabel("Occurence")
 plt.title("Class Distribution")
 plt.show()
+
+#show bar graph for class distribution per fold
+
+class_nums = []
+def get_all_labels(sub_dir):
+    files = glob(os.path.join(folded_dir,sub_dir,'*.wav'))
+    for eachfile in files:
+        arr = eachfile.split("-")
+        class_no = str(arr[2])[0]
+        class_nums.append(class_no)
+    fixed = [0,1,2,3,4,5,6,7,8,9]
+    temp = []
+    for element in fixed:
+        temp.append(class_nums.count(str(element)))
+    class_nums.clear()
+    return temp
+
+dict = {}
+for i in range(10):
+    dict['fold'+str(i)] = get_all_labels('fold'+str(i))
+print(dict)
+
+
+N = 10
+ind = np.arange(N) 
+width = 0.08
+ 
+fold0 = dict['fold0']
+fold1= dict['fold1']
+fold2= dict['fold2']
+fold4= dict['fold3']
+fold3= dict['fold4']
+fold5= dict['fold5']
+fold6= dict['fold6']
+fold7= dict['fold7']
+fold8= dict['fold8']
+fold9 = dict['fold9']
+
+bar1 = plt.bar(ind, fold0, width, color = 'r')
+bar2 = plt.bar(ind+width, fold1, width, color='g')
+bar3 = plt.bar(ind+width*2, fold2, width, color = 'b')
+bar4 = plt.bar(ind+width*3, fold3, width, color = 'c')
+bar5 = plt.bar(ind+width*4, fold4, width, color='m')
+bar6 = plt.bar(ind+width*5, fold5, width, color = 'y')
+bar7 = plt.bar(ind+width*6, fold6, width, color = 'k')
+bar8 = plt.bar(ind+width*7, fold7, width, color='#FE3377')
+bar9 = plt.bar(ind+width*8, fold8, width, color = '#33FEB1')
+bar10 = plt.bar(ind+width*9, fold9, width, color = '#FEBA33')
+  
+plt.xlabel("Fold Number")
+plt.ylabel('Class ID Occurances')
+plt.title("Number of each Class ID Within Each Fold")
+  
+plt.xticks(ind+width*5,['fold0','fold1','fold2','fold3','fold4','fold5','fold6','fold7','fold8','fold9'])
+plt.legend((bar1,bar2, bar3,bar4,bar6,bar6,bar7,bar8,bar9,bar10), ("0-Voices","1-Locomotion","2-Digestive","3-Elements","4-Animals","5-Cook_App","6-Clean-App","7-Vent_App","8-Furniture","9-Instruments"),fontsize=6)
+plt.show()
+
+
